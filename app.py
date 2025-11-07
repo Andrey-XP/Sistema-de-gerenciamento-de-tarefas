d9afbe23c506483fe5278c80e16109e6be77ebf4
Andrey-XP <andrey.sousalipe@gmail.com>
2025-11-07 00:30:02 -0300

    mudanças

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-ta = 
+while True:
+    nome = input('Digite Seu Nome: ').strip()
+    if len(nome) == 0:
+        print('Erro: O nome não pode ficar vazio.\n')
+        continue
+    if any(char.isdigit() for char in nome):
+        print('Erro: O nome não pode conter numeros.\n')
+        continue
+    print('Nome valisdo')
+    break
+while True:
+    email = input('Digite Seu Email')
+    if "@" not in email or "." not in email:
+        print('Erro: Email Invalido.\n')
+        continue
+    print('Email Valido!\n')
+    break
+while True:
+    senha = input("Crie uma senha (mínimo 6 caracteres): ").strip()
+
+    if len(senha) < 6:                          
+        
+        print("Erro: a senha deve ter pelo menos 6 caracteres.\n")
+        continue
+
+    if " " in senha:                            
+        print("Erro: a senha não pode conter espaços.\n")
+        continue
+
+    print("✅ Senha cadastrada com sucesso!\n")
+    break
+
+
+
+print("🎉 Cadastro concluído com sucesso!")
